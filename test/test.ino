eade550d8291d01511142e14205f17023468b30c
Sergey Dunets <dunets.devel@gmail.com>
2012-11-26 11:17:19 +0200

    add delays for debugging

--- test/test.ino
+++ test/test.ino
@@ -25,7 +25,9 @@ void loop() {
    // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd->setCursor(0, 1);
+  delay(100);
   // print the number of seconds since reset:
   lcd->print(millis()/1000);
   //utils.digitalWriteToShiftRegister(6, HIGH);
+  delay(100);
 }
